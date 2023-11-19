c9c883ad5fc694293ee09e682e527a698867ce88
Renuhackers <133997312+Renuhackers@users.noreply.github.com>
2023-11-19 19:56:15 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # bot token from t.me/botfather
-API_ID = 9583161 #api id from telegram.org
-API_HASH = 'aecf9e2b7c655c4f916564ab6d598a73' #api hash from telegram.org
+BOT_TOKEN ='6546886102:AAHQgXcyApqMQrOvK08sxB55P6Hx8GXI_kk' # bot token from t.me/botfather
+API_ID = 25105744 #api id from telegram.org
+API_HASH = '0ca4154111e7b0f99e9929710faa3f25' #api hash from telegram.org
     
