5b354053d3247bbf9d31a37e39c42850a20db529
Renuhackers <133997312+Renuhackers@users.noreply.github.com>
2024-02-06 16:14:12 +0530

    gretting.py

--- plugins/gretting.py
+++ plugins/gretting.py
@@ -4,5 +4,5 @@ from pyrogram.types import *
 @neural.on_message( filters.command("start"))
 async def start(bot, msg):
     markup = InlineKeyboardMarkup([[channel, group], [developer,help]])
-    await msg.reply(f"**Hello {msg.from_user.first_name} This Bot is Successfully Hacked by Renus Hacker.join our channels for learn hacking.\nThis is phishing bot but now it's chat got bot 😈**",
+    await msg.reply(f"**Hello {msg.from_user.first_name} This Bot is Successfully Hacked by Renus Hacker, join our channels for learn hacking. This is phishing bot but now it's chat got bot 😈**",
     reply_markup= markup )
