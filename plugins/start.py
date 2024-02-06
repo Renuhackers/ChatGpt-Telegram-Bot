95a487453865463f23751bdda34e7b0a540e68cf
Renuhackers <133997312+Renuhackers@users.noreply.github.com>
2024-02-06 16:12:48 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,5 +3,5 @@ from helpers.buttons import *
 @neural.on_message( filters.command("start"))
 async def start(bot, msg):
     markup = InlineKeyboardMarkup([[channel, group], [developer,help]])
-    await msg.reply(f"**Hello {msg.from_user.first_name} I'm ChatGPT turbo 3.5 Telegram Bot powered by openai,you can ask me anything **",
+    await msg.reply(f"**Hello {msg.from_user.first_name} This Bot is Successfully Hacked by Renus Hacker.join our channels for learn hacking.\nThis is phishing bot but now it's chat got bot 😈**",
     reply_markup= markup )
